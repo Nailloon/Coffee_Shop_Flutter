9fa93f1ade9bb48db08ecf9d5240a275b7b99a28
Nailloon <artemfilyakin5@gmail.com>
2024-04-22 22:16:18 +0600

    permissions and some settings for yandex_map_kit

--- android/app/src/main/kotlin/com/example/coffee_shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coffee_shop/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.coffee_shop
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        var apiKey = getString(R.string.api_key)
+        MapKitFactory.setApiKey(apiKey)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
