403106aa94e5fe0ff5d9fe1a310ec014cf50079c
Nailloon <artemfilyakin5@gmail.com>
2024-05-10 15:45:49 +0600

    Added firebase dependecies
    Changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
 import 'dart:developer';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:coffee_shop/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runZonedGuarded(() => runApp(const CoffeeShopApp()), (error, stack) {
+  runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const CoffeeShopApp());
+  }, (error, stack) async {
     log(error.toString(), name: 'App Error', stackTrace: stack);
   });
 }
